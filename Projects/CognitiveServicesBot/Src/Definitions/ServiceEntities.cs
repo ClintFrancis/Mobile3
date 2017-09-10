dede4abec5e6c9be6bb55fbeba85e314190bd518
Clinton Francis <clfranci@microsoft.com>
2017-09-10 08:42:03 +0100

    Bug Fixes Etc

--- Projects/CognitiveServicesBot/Src/Definitions/ServiceEntities.cs
+++ Projects/CognitiveServicesBot/Src/Definitions/ServiceEntities.cs
@@ -7,9 +7,6 @@ namespace CognitiveServicesBot
 {
     public static class ServiceEntities
     {
-        public const string QuerySearch = "cognitiveservice.query.search";
-        public const string QueryDocumentation = "cognitiveservice.query.documentation";
-        public const string Documentation = "cognitiveservice.documentation";
         public const string Help = "cognitiveservice.help";
         public const string Product = "cognitiveservice.product";
         public const string Search = "cognitiveservice.search";
